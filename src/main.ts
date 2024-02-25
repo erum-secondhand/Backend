120fd96ef54d71741a0d9a21518f54effbb8cca6
희은 <datakubit2@gmail.com>
2024-02-26 03:12:20 +0900

    fix: 쿠키에 대한 CORS 허용

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
 
   app.use(
     session({
