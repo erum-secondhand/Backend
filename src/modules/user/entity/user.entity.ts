123e3c88b8701d13d06ab5f7812d00945c493d4f
희은 <datakubit2@gmail.com>
2024-08-24 17:36:07 +0900

    refactor: User 엔티티 comment 추가

--- src/modules/user/entity/user.entity.ts
+++ src/modules/user/entity/user.entity.ts
@@ -6,18 +6,18 @@ export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ unique: true })
+  @Column({ unique: true, comment: "사용자 이메일" })
   email: string;
 
-  @Column()
+  @Column({ comment: "사용자 비밀번호" })
   password: string;
 
-  @Column()
+  @Column({ comment: "사용자 이름" })
   name: string;
 
-  @Column()
+  @Column({ comment: "사용자 학번"})
   studentId: string;
 
-  @Column()
+  @Column({ comment: "사용자 전공"})
   major: string;
 }
