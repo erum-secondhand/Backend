eec3b165076c3ab4a0eb887c01ab727e83962230
희은 <datakubit2@gmail.com>
2024-09-05 01:04:11 +0900

    fix: 경로 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { BookModule } from 'modules/book/book.module';
-import { MysqlModule } from 'config/mysql/mysql.module';
+import { BookModule } from './modules/book/book.module';
+import { MysqlModule } from './config/mysql/mysql.module';
 import { UserModule } from 'modules/user/user.module';
 import { ChatModule } from 'modules/chat/chat.module';
-import { AppController } from 'app.controller';
-import { AppService } from 'app.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UserModule, BookModule, ChatModule, MysqlModule],
