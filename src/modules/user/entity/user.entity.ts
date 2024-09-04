d394bb5381f0e39be18063c4a53c4cd127bc9fbe
희은 <datakubit2@gmail.com>
2024-09-04 20:51:40 +0900

    fix: 유저의 민감한 정보 Response 방지

--- src/modules/user/entity/user.entity.ts
+++ src/modules/user/entity/user.entity.ts
@@ -1,23 +1,25 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { BaseEntity } from 'src/global/common/base.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ unique: true, comment: "사용자 이메일" })
+  @Column({ unique: true, comment: '사용자 이메일' })
   email: string;
 
-  @Column({ comment: "사용자 비밀번호" })
+  @Column({ comment: '사용자 비밀번호' })
+  @Exclude()
   password: string;
 
-  @Column({ comment: "사용자 이름" })
+  @Column({ comment: '사용자 이름' })
   name: string;
 
-  @Column({ comment: "사용자 학번"})
+  @Column({ comment: '사용자 학번' })
   studentId: string;
 
-  @Column({ comment: "사용자 전공"})
+  @Column({ comment: '사용자 전공' })
   major: string;
 }
